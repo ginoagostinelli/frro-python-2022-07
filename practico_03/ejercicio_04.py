d4314bc28a8fb580b95ce84de54c475460def3b0
Gino Agostinelli <ginoagostinelli5@gmail.com>
2022-06-08 22:00:42 -0300

    Ejercicios practica 03 terminados

--- practico_03/ejercicio_04.py
+++ practico_03/ejercicio_04.py
@@ -19,11 +19,19 @@ class Perro(Animal):
     """
     # Completar
 
+    def __init__(self, edad:int = 0, raza: str = ""):
+        super().__init__(edad)
+        self.raza: str = raza
+
+    def descripcion(self) -> str:
+        return f'Soy un perro y {super().descripcion().lower()}'
+
 
 # NO MODIFICAR - INICIO
 terrier = Perro(edad=8, raza="Yorkshire Terrier")
 cachorro = Perro(edad=1)
 dogo = Perro(raza="Dogo")
+terrier.descripcion()
 
 assert Animal(10).descripcion() == "Tengo 10 años"
 assert terrier.descripcion() == "Soy un perro y tengo 8 años"
@@ -40,11 +48,20 @@ from dataclasses import dataclass
 @dataclass
 class Animal:
     pass # Completar
+    
+    edad: int = 0
+
+    def descripcion(self) -> str:
+        return f"Tengo {self.edad} años"
 
 
 @dataclass
 class Perro(Animal):
     pass # Completar
+    raza: str = ""
+
+    def descripcion(self) -> str:
+        return f'Soy un perro y {super().descripcion().lower()}'
 
 
 # NO MODIFICAR - INICIO
